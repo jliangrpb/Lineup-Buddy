168b6387d0f27ff8bb34414cb1d44f7a687eb4a5
jliangrpb <jeremy.liang612@gmail.com>
2023-05-10 17:35:14 -0400

    Finished JS Project, not finalized, can still add some extra functionalities

--- src/index.js
+++ src/index.js
@@ -9,6 +9,25 @@ let selectedAgent = "Brimstone"
 
 displayMapInfo(selectedMap);
 
+document.addEventListener('DOMContentLoaded', function() {
+  const modalIcon = document.getElementById('modalIcon');
+  const modal = document.getElementById('modal');
+  const closeModalButton = document.getElementById('closeModalButton');
+
+  // Function to open the modal
+  function openModal() {
+    modal.style.display = 'block';
+  }
+
+  // Function to close the modal
+  function closeModal() {
+    modal.style.display = 'none';
+  }
+
+  modalIcon.addEventListener('click', openModal);
+  closeModalButton.addEventListener('click', closeModal);
+});
+
 
 
 
